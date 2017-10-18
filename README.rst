4a698153f1c532bbbee7665ea2262d3d58533757
vaideesg <vaidees@gmail.com>
2017-10-18 21:57:08 +0530

    License and Branding Changes.

--- README.rst
+++ README.rst
@@ -1,8 +1,7 @@
-Dell OpenManage SDK
-=======================
+Dell EMC OpenManage Python SDK
+==============================
 
-Provides information about the OpenManage SDK. Useful for connecting to iDRAC,
-Force10, OME and Nagios
+Dell EMC OpenManage Python Software Development Kit (SDK) is a pythonic library for programatically managing Dell EMC Servers and Chassis. Using this library a user can perform lifecycle operations on Dell EMC PowerEdge Servers and Blade Enclosure Chassis in Pythonic way. The Dell EMC OpenManage Python SDK leverages DMTF's Web Services Management standard (WS-Man), IETF's Simple Network Management Protocol (SNMP) and the DMTF Redfish standard for managing and monitoring the Dell EMC PowerEdge branded hardware.
 
 ----
 
--- setup-omsdk.py
+++ setup-omsdk.py
@@ -58,7 +58,7 @@ setup(
     author_email='vaideeswaran_ganesan@dell.com',
 
     # Choose your license
-    license='MIT',
+    license='GPLv3',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -66,14 +66,14 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
